06830aaed441907e9973557398db1b60952758eb
j22868706 <j22868706@gmail.com>
2024-03-03 22:13:43 -0600

    added conditionalContentAndHook

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/Header/TabButton.jsx";
+import {EXAMPLES} from "./data.js"
 
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function selectHandler(selectedButton){
+    setSelectedTopic(selectedButton)
+  }
+
   return (
     <div>
       <Header />
@@ -25,11 +33,23 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={()=> selectHandler("components")}>Components</TabButton>
+            <TabButton onSelect={()=> selectHandler("jsx")}>JSX</TabButton>
+            <TabButton onSelect={()=> selectHandler("props")}>Props</TabButton>
+            <TabButton onSelect={()=> selectHandler("state")}>State</TabButton>
           </menu>
+          {!selectedTopic ? (<p>Please select a topic</p> 
+          ) : (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>
+                {EXAMPLES[selectedTopic].code}
+                </code>
+              </pre>
+            </div>
+          )}
         </section>
       </main>
     </div>
